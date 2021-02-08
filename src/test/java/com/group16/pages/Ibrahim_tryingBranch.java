622e35909245ce0c58be0d9b89f7182803072c9b
Ibrahim224466 <mercanibrahim@hotmail.com>
2021-02-08 16:46:53 +0100

    Ibrahim_secondcommit

--- src/test/java/com/group16/pages/Ibrahim_tryingBranch.java
+++ src/test/java/com/group16/pages/Ibrahim_tryingBranch.java
@@ -1,9 +1,12 @@
 package com.group16.pages;
 
+import java.sql.SQLOutput;
+
 public class Ibrahim_tryingBranch {
 
     public static void main(String[] args) {
         System.out.println("Hello group");
+        System.out.println("Hello second time");
     }
 
 }
