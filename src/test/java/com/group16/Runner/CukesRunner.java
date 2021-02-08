608a4b4896e24f1efa9015c3cb86cf97b879bc3c
Rustam Jafarov <j.rustam@hotmail.com>
2021-02-08 16:10:57 +0100

    initial commit

---
+++ src/test/java/com/group16/Runner/CukesRunner.java
@@ -0,0 +1,19 @@
+package com.group16.Runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+       plugin = {"json:target/cucumber.json",
+       "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/group16/StepDefs",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
+
---
+++ src/test/java/com/group16/pages/ForTheSakeOfPractice.java
@@ -0,0 +1,7 @@
+package com.group16.pages;
+
+public class ForTheSakeOfPractice {
+    public static void main(String[] args) {
+        System.out.println("Github team collaboration practice");
+    }
+}
