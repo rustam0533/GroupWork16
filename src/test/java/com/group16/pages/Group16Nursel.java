6bd5433bd79698ab062a9f6d245d2607353d7c72
nrslc <cankarnursel@gmail.com>
2021-02-08 16:46:05 +0100

    Nursel First Commit

---
+++ src/test/java/com/group16/pages/Group16Nursel.java
@@ -0,0 +1,15 @@
+package com.group16.pages;
+
+public class Group16Nursel {
+    // bu bir group denemesidir
+
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
